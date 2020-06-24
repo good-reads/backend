e6e9e5461ffffcb305f85c35bdfc7c2db4d3f1f9
kjw217 <rlawndhks217@gmail.com>
2020-06-24 23:39:30 +0900

    split database settings

--- config/settings/deploy.py
+++ config/settings/deploy.py
@@ -8,12 +8,12 @@ ALLOWED_HOSTS = [
     "54.180.154.184",
 ]
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # # AWS settings
 # AWS_ACCESS_KEY_ID = config_secret['aws']['access_key_id'] # .csv 파일에 있는 내용을 입력 Access key ID
