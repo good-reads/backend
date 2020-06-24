bbc1804715d0c45e5506e2995a8567289afd573f
kjw217 <rlawndhks217@gmail.com>
2020-06-24 23:03:58 +0900

    split AWS storage settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -154,16 +154,16 @@ BOOK_LIST_LIMITAION = 10
 MEDIA_URL = '/storage/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'storage/')
 
-# AWS settings
-AWS_ACCESS_KEY_ID = config_secret['aws']['access_key_id'] # .csv 파일에 있는 내용을 입력 Access key ID
-AWS_SECRET_ACCESS_KEY = config_secret['aws']['secret_key'] # .csv 파일에 있는 내용을 입력 Secret access key
-AWS_REGION = config_secret['aws']['region']
-
-# S3 Storages settings
-AWS_STORAGE_BUCKET_NAME = config_secret['aws']['storage_name'] # 설정한 버킷 이름
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.%s.amazonaws.com' % (AWS_STORAGE_BUCKET_NAME,AWS_REGION)
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',
-}
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# # AWS settings
+# AWS_ACCESS_KEY_ID = config_secret['aws']['access_key_id'] # .csv 파일에 있는 내용을 입력 Access key ID
+# AWS_SECRET_ACCESS_KEY = config_secret['aws']['secret_key'] # .csv 파일에 있는 내용을 입력 Secret access key
+# AWS_REGION = config_secret['aws']['region']
+#
+# # S3 Storages settings
+# AWS_STORAGE_BUCKET_NAME = config_secret['aws']['storage_name'] # 설정한 버킷 이름
+# AWS_S3_CUSTOM_DOMAIN = '%s.s3.%s.amazonaws.com' % (AWS_STORAGE_BUCKET_NAME,AWS_REGION)
+# AWS_S3_OBJECT_PARAMETERS = {
+#     'CacheControl': 'max-age=86400',
+# }
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
